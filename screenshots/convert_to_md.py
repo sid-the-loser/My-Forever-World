d385e77e52e60d1a3fbc9b98269a4eb59732f283
Sidharth <sidharthsuresh07@gmail.com>
2026-02-14 15:34:55 -0500

    2026-02-14 15:34:55

--- screenshots/convert_to_md.py
+++ screenshots/convert_to_md.py
@@ -8,7 +8,7 @@ for item in sorted_dir_list:
     if os.path.isfile(item) and item.endswith(".png"):
         date_n_time = item[:-4].split("_")
 
-        md_data += f">![{item}]({item})\n>Filename: {item}\n>Date (YYYY-MM-DD): {date_n_time[0]}\n>Time (HH.MM.SS): {date_n_time[1]}\n\n"
+        md_data += f">![{item}]({item})\n>\n>Filename: {item}\n>\n>Date (YYYY-MM-DD): {date_n_time[0]}\n>\n>Time (HH.MM.SS): {date_n_time[1]}\n\n"
 
 with open("screenshots.md", "w") as f:
     f.write(md_data)
